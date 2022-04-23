3cc1cd86cf3191b256aaaac4a63b1d16ade40e1d
Yaman Qassas <yaman102011@gmail.com>
2022-04-24 02:30:21 +0300

    Make auth routes public

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
+import { Public } from './public.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -8,11 +9,13 @@ export class AuthController {
 		private authService: AuthService
 	) { }
 
+	@Public()
 	@Post('register')
 	async register(@Body() user) {
 		return this.authService.register(user);
 	}
 
+	@Public()
 	@UseGuards(LocalAuthGuard)
 	@Post('login')
 	async login(@Request() req) {
