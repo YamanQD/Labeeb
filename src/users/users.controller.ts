4b86c80a5f259587e68817052112d37e8e28834a
Yaman Qassas <yaman102011@gmail.com>
2022-04-23 16:46:27 +0300

    Remove imports and usage of deleted files

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,12 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  create(@Body() createUserDto) {
     return this.usersService.create(createUserDto);
   }
 
@@ -23,7 +21,7 @@ export class UsersController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  update(@Param('id') id: string, @Body() updateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
