0e93d61b34ca07886792aadd1d3e727d60071356
Yaman Qassas <yaman102011@gmail.com>
2022-04-24 00:15:31 +0300

    Remove create user controller route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,9 +5,4 @@ import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
-
-  @Post()
-  create(@Body() user: User) {
-    return this.usersService.create(user);
-  }
 }
