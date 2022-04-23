68d75e0420a0476b363eaecf4a83d4485949b392
Yaman Qassas <yaman102011@gmail.com>
2022-04-23 23:57:28 +0300

    Add a proper profile route with jwt authorization

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return this.appService.getProfile(req.user);
+  }
 
   @Get()
   getHello(): string {
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './users/user.entity';
+import { UsersService } from './users/users.service';
 
 @Injectable()
 export class AppService {
+  constructor(private usersService: UsersService) { }
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getProfile(user: User) {
+    const { password, ...profile } = await this.usersService.findOne(user.username);
+    return profile;
   }
 }
