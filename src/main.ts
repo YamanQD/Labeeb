a34eddded8f010d8121b3f31e5492753d5720f48
Yaman Qassas <yaman102011@gmail.com>
2022-04-24 00:13:34 +0300

    Setup Swagger Nestjs Module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Labeeb')
+    .setDescription('The labeeb API description')
+    .setVersion('1.0')
+    .addTag('labeeb')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
