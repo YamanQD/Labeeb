870ad870dad05aaa9b8503917c69c736f8810dbb
Yaman Qassas <yaman102011@gmail.com>
2022-05-09 06:30:25 +0300

    BE: Remove user controller from users module file

--- packages/server/src/users/users.module.ts
+++ packages/server/src/users/users.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService]
 })
