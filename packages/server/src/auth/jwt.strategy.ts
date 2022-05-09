96d27c3235516891c6e3bad8af8d9da615d1f2eb
Yaman Qassas <yaman102011@gmail.com>
2022-05-09 06:59:37 +0300

    BE: Rename `userId` field in JWT to `id`

--- packages/server/src/auth/jwt.strategy.ts
+++ packages/server/src/auth/jwt.strategy.ts
@@ -14,6 +14,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 	}
 
 	async validate(payload: any) {
-		return { userId: payload.sub, username: payload.username, role: payload.role };
+		return { id: payload.sub, username: payload.username, role: payload.role };
 	}
 }
