62848a4689a4352e23e96d9f615b2b46c974930f
Hasan Mothaffar <hasan.mozafar@gmail.com>
2022-07-03 10:46:12 +0300

    FE: Satisfy TS's compiler again

--- packages/client/src/features/tasks/services/taskService.ts
+++ packages/client/src/features/tasks/services/taskService.ts
@@ -1,9 +1,10 @@
 import { ITasksRepository, TaskGroupFilters } from "../domain/ItaskRepository";
+import { ITaskGroup } from "../domain/task";
 
 export class TasksService {
     constructor(private tasksRepository: ITasksRepository) {}
 
-    public async getTaskGroupsForProject({ projectId, groupId }: TaskGroupFilters) {
+    public async getTaskGroupsForProject({ projectId, groupId }: TaskGroupFilters): Promise<ITaskGroup[]> {
         return this.tasksRepository.getTaskGroups({
             projectId,
             groupId
