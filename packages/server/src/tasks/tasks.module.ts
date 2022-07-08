d823544c51ff518d0478f375bf0a5eafbb8cd576
Yaman Qassas <yaman102011@gmail.com>
2022-07-08 21:19:06 +0300

    BE: Register Project TypeORM Repo in tasks module

--- packages/server/src/tasks/tasks.module.ts
+++ packages/server/src/tasks/tasks.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Project } from 'src/projects/project.entity';
 import { Task } from './task.entity';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Project, Task])],
   controllers: [TasksController],
   providers: [TasksService]
 })
