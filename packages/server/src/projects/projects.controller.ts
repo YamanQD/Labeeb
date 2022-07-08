fdd1ca2771f41743bcc9fbeb460d239a2e339281
Yaman Qassas <yaman102011@gmail.com>
2022-07-08 21:23:35 +0300

    BE: Implement project creation

--- packages/server/src/projects/projects.controller.ts
+++ packages/server/src/projects/projects.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, } from '@nestjs/common';
+import { Task } from 'src/tasks/task.entity';
+import { Project } from './project.entity';
+import { ProjectsService } from './projects.service';
 
 @Controller('projects')
-export class ProjectsController {}
+export class ProjectsController {
+	constructor(private readonly projectsService: ProjectsService) { }
+
+
+	@Post()
+	async create(@Body() body: any): Promise<Project> {
+		return await this.projectsService.create(body);
+	}
+
+	// }
+}
--- packages/server/src/projects/projects.service.ts
+++ packages/server/src/projects/projects.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from 'src/tasks/task.entity';
+import { Repository } from 'typeorm';
+import { Project } from './project.entity';
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+	constructor(
+		@InjectRepository(Project)
+		private readonly projectRepository: Repository<Project>
+	) { }
+
+
+	async create(project: Project): Promise<Project> {
+		return await this.projectRepository.save(project);
+	}
+}
