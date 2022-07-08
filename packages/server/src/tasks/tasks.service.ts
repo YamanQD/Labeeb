34df48a8cce357d40742b1481409b7426274578b
Yaman Qassas <yaman102011@gmail.com>
2022-07-08 21:27:26 +0300

    BE: Modify task creation to include project field

--- packages/server/src/tasks/tasks.service.ts
+++ packages/server/src/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Project } from 'src/projects/project.entity';
 import { Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
@@ -9,7 +10,9 @@ import { Task } from './task.entity';
 export class TasksService {
 	constructor(
 		@InjectRepository(Task)
-		private readonly taskRepository: Repository<Task>
+		private readonly taskRepository: Repository<Task>,
+		@InjectRepository(Project)
+		private readonly projectRepository: Repository<Project>
 	) { }
 
 	async findAll(): Promise<Task[]> {
@@ -25,12 +28,16 @@ export class TasksService {
 	}
 
 	async create(body: CreateTaskDto, userId: any): Promise<Task> {
-		const newTask = this.taskRepository.create({
+		const project = await this.projectRepository.findOne({ where: { id: body.projectId } });
+
+		const task = this.taskRepository.create({
 			created_by: userId,
 			createdAt: new Date(),
+			project: project,
 			...body
 		});
-		return await this.taskRepository.save(newTask);
+
+		return await this.taskRepository.save(task);
 	}
 
 	async update(id: number, body: UpdateTaskDto): Promise<Task> {
