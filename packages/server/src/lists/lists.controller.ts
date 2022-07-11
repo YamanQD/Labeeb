70040cf61c3347b8c0ee12f27820b764bf1c0298
Yaman Qassas <yaman102011@gmail.com>
2022-07-11 10:11:46 +0300

    BE: Purge lists controller routes

---
+++ packages/server/src/lists/lists.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { List } from './list.entity';
+import { ListsService } from './lists.service';
+
+@Controller('lists')
+export class ListsController {
+	constructor(private readonly listsService: ListsService) { }
+
+	@Post()
+	async create(@Body() body: any): Promise<List> {
+		return await this.listsService.create(body);
+	}
+}
