9376df5917b66f2ad9554abf9b71118cb32d1aa9
Yaman Qassas <yaman102011@gmail.com>
2022-07-11 11:20:22 +0300

    BE: Add find list tasks route

--- packages/server/src/lists/lists.controller.ts
+++ packages/server/src/lists/lists.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Task } from 'src/tasks/task.entity';
 import { CreateListDto } from './dto/create-list-dto';
 import { List } from './list.entity';
 import { ListsService } from './lists.service';
@@ -6,6 +7,11 @@ import { ListsService } from './lists.service';
 @Controller('lists')
 export class ListsController {
 	constructor(private readonly listsService: ListsService) { }
+
+	@Get(':id')
+	async findListTasks(@Param('id') id: number): Promise<Task[]> {
+		return await this.listsService.findListTasks(id);
+	}
 
 	@Post()
 	async create(@Body() body: CreateListDto): Promise<List> {
