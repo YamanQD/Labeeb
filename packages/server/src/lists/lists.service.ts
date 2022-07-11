69168493cf1b6b03e2af145a47083e006a10b5ed
Yaman Qassas <yaman102011@gmail.com>
2022-07-11 10:36:31 +0300

    BE: Implement find list tasks

--- packages/server/src/lists/lists.service.ts
+++ packages/server/src/lists/lists.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Project } from 'src/projects/project.entity';
+import { Task } from 'src/tasks/task.entity';
 import { Repository } from 'typeorm';
 import { List } from './list.entity';
 
@@ -15,5 +16,17 @@ export class ListsService {
 
 	async create(list: List): Promise<List> {
 		return await this.listRepository.save(list);
+	}
+
+	async findListTasks(id: number): Promise<Task[]> {
+		const list = await this.listRepository.findOne({
+			where: { id },
+			relations: { tasks: true }
+		});
+		if (!list) {
+			throw new NotFoundException('List not found');
+		}
+
+		return list.tasks;
 	}
 }
