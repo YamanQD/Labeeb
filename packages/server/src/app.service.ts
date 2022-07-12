05ec5372425043e4e3fcd51e114b845411504cbe
Yaman Qassas <yaman102011@gmail.com>
2022-07-12 15:02:27 +0300

    BE: Implement user seeding

--- packages/server/src/app.service.ts
+++ packages/server/src/app.service.ts
@@ -1,13 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { TasksService } from './tasks/tasks.service';
 import { User } from './users/user.entity';
 import { UsersService } from './users/users.service';
 
 @Injectable()
-export class AppService {
-  constructor(private usersService: UsersService) { }
+export class AppService implements OnApplicationBootstrap {
+  constructor(
+    private usersService: UsersService
+  ) { }
 
   async getProfile(user: User) {
     const { password, ...profile } = await this.usersService.findOne(user.username);
     return profile;
+  }
+
+  async onApplicationBootstrap() {
+    await this.usersService.seed();
   }
 }
--- packages/server/src/users/users.service.ts
+++ packages/server/src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Role } from 'src/enums/role.enum';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 
@@ -17,5 +18,33 @@ export class UsersService {
 
   async findOne(username: string) {
     return await this.usersRepository.findOne({ where: { username } });
+  }
+
+  async seed() {
+    const allUsers = await this.usersRepository.find();
+
+    if (allUsers.filter((user) => user.username === 'admin').length === 0) {
+      const admin = new User();
+      admin.username = 'admin';
+      admin.password = 'admin';
+      admin.role = Role.ADMIN;
+      await this.usersRepository.save(admin);
+    }
+
+    if (allUsers.filter((user) => user.username === 'Yaman').length === 0) {
+      const user = new User();
+      user.username = 'Yaman';
+      user.password = 'Yaman';
+      user.role = Role.USER;
+      await this.usersRepository.save(user);
+    }
+
+    if (allUsers.filter((user) => user.username === 'Hasan').length === 0) {
+      const user = new User();
+      user.username = 'Hasan';
+      user.password = 'Hasan';
+      user.role = Role.USER;
+      await this.usersRepository.save(user);
+    }
   }
 }
