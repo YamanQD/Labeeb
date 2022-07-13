14cff09ad65d5da8c574a8d0770e1cab1d6ac38d
Yaman Qassas <yaman102011@gmail.com>
2022-07-13 10:34:53 +0300

    BE: Use seeders in app service

--- packages/server/src/app.service.ts
+++ packages/server/src/app.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { ListsService } from './lists/lists.service';
+import { ProjectsService } from './projects/projects.service';
 import { TasksService } from './tasks/tasks.service';
 import { User } from './users/user.entity';
 import { UsersService } from './users/users.service';
@@ -6,7 +8,10 @@ import { UsersService } from './users/users.service';
 @Injectable()
 export class AppService implements OnApplicationBootstrap {
   constructor(
-    private usersService: UsersService
+    private usersService: UsersService,
+    private tasksService: TasksService,
+    private listsService: ListsService,
+    private projectsService: ProjectsService,
   ) { }
 
   async getProfile(user: User) {
@@ -16,5 +21,8 @@ export class AppService implements OnApplicationBootstrap {
 
   async onApplicationBootstrap() {
     await this.usersService.seed();
+    await this.projectsService.seed();
+    setTimeout(async () => await this.listsService.seed(), 1000);
+    setTimeout(async () => await this.tasksService.seed(), 2000);
   }
 }
