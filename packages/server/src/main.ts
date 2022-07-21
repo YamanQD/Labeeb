e35aa3ba560204ee9ce32e11d165bc6a6ab9609a
Hasan Mothaffar <hasan.mozafar@gmail.com>
2022-07-21 11:03:48 +0300

    BE: Enable CORS so the frontend can reach out to backend

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+	app.enableCors();
 
 	const config = new DocumentBuilder()
 		.setTitle('Labeeb')
