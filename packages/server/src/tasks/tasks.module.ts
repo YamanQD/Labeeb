21e9e6d2d5a77560aad2f9ebf000f25f40cf15e4
Yaman Qassas <yaman102011@gmail.com>
2022-08-05 19:42:57 +0300

    BE: Use user repository in tasks module

--- packages/server/src/tasks/tasks.module.ts
+++ packages/server/src/tasks/tasks.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { List } from 'src/lists/list.entity';
 import { Status } from 'src/projects/status.entity';
 import { Tag } from 'src/projects/tags.entity';
+import { User } from 'src/users/user.entity';
 import { Task } from './task.entity';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([List, Task, Status, Tag])],
+	imports: [TypeOrmModule.forFeature([List, Task, Status, Tag, User])],
 	controllers: [TasksController],
 	providers: [TasksService],
 	exports: [TasksService],
