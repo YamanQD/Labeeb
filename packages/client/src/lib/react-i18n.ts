8d510872a988a8872ee7027e232d34b40b01c44d
Rami Sabbagh <rami.sab07@gmail.com>
2022-08-06 20:47:01 +0300

    FE: Remove `i18next-http-backend` and bundle the locales with the app

--- packages/client/src/lib/react-i18n.ts
+++ packages/client/src/lib/react-i18n.ts
@@ -1,16 +1,21 @@
 import i18n from "i18next";
-import HttpBackend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
-i18n.use(HttpBackend)
-    .use(initReactI18next)
-    .init({
-        lng: "en",
-        fallbackLng: "en",
-        interpolation: {
-            escapeValue: false,
+import localeAR from "../locales/ar.json";
+import localeEN from "../locales/en.json";
+
+i18n.use(initReactI18next).init({
+    lng: "en",
+    fallbackLng: "en",
+    resources: {
+        en: {
+            translation: localeEN,
         },
-        backend: {
-            loadPath: "/locales/{{lng}}.json",
+        ar: {
+            translation: localeAR,
         },
-    });
+    },
+    interpolation: {
+        escapeValue: false,
+    },
+});
