41485f8cdf7b4692d06e4f14a481114fed776bd7
Hasan Mothaffar <hasan.mozafar@gmail.com>
2022-08-11 10:17:15 +0300

    FE: Clean up the 404 page

--- packages/client/src/pages/404.tsx
+++ packages/client/src/pages/404.tsx
@@ -1,15 +1,4 @@
-import SearchTwoToneIcon from "@mui/icons-material/SearchTwoTone";
-import {
-    Box,
-    Button,
-    Card,
-    Container,
-    Divider,
-    FormControl,
-    InputAdornment,
-    OutlinedInput,
-    Typography,
-} from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 const MainContent = styled(Box)(
@@ -24,18 +13,6 @@ const MainContent = styled(Box)(
 `
 );
 
-const OutlinedInputWrapper = styled(OutlinedInput)(
-    ({ theme }) => `
-    background-color: ${theme.palette.background.paper};
-`
-);
-
-const ButtonSearch = styled(Button)(
-    ({ theme }) => `
-    margin-right: -${theme.spacing(1)};
-`
-);
-
 const Status404 = () => {
     return (
         <>
@@ -46,42 +23,11 @@ const Status404 = () => {
                         <Typography variant="h2" sx={{ my: 2 }}>
                             The page you were looking for doesn't exist.
                         </Typography>
-                        <Typography
-                            variant="h4"
-                            color="text.secondary"
-                            fontWeight="normal"
-                            sx={{ mb: 4 }}
-                        >
-                            It's on us, we moved the content to a different page. The search below
-                            should help!
-                        </Typography>
+
+                        <Button href="/" variant="contained">
+                            Go to homepage
+                        </Button>
                     </Box>
-                    <Container maxWidth="sm">
-                        <Card sx={{ textAlign: "center", mt: 3, p: 4 }}>
-                            <FormControl variant="outlined" fullWidth>
-                                <OutlinedInputWrapper
-                                    type="text"
-                                    placeholder="Search terms here..."
-                                    endAdornment={
-                                        <InputAdornment position="end">
-                                            <ButtonSearch variant="contained" size="small">
-                                                Search
-                                            </ButtonSearch>
-                                        </InputAdornment>
-                                    }
-                                    startAdornment={
-                                        <InputAdornment position="start">
-                                            <SearchTwoToneIcon />
-                                        </InputAdornment>
-                                    }
-                                />
-                            </FormControl>
-                            <Divider sx={{ my: 4 }}>OR</Divider>
-                            <Button href="/overview" variant="outlined">
-                                Go to homepage
-                            </Button>
-                        </Card>
-                    </Container>
                 </Container>
             </MainContent>
         </>
