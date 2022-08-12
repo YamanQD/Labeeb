5cec81b1604543acaf5097f67dffab00cbc3c5b4
Yaman Qassas <yaman102011@gmail.com>
2022-08-12 09:51:49 +0300

    BE: Allow project users to access its lists

--- packages/server/src/lists/lists.service.ts
+++ packages/server/src/lists/lists.service.ts
@@ -15,6 +15,15 @@ export class ListsService {
 		private readonly projectRepository: Repository<Project>,
 	) { }
 
+	async isProjectUser(listId: number, userId: number): Promise<boolean> {
+		const list = await this.listRepository.findOne({ where: { id: listId }, relations: ['project', 'project.users'] });
+		if (!list) {
+			throw new NotFoundException(['List not found']);
+		}
+
+		return list.project?.users?.some(({ id }) => id === userId);
+	}
+
 	async create(list: CreateListDto): Promise<List> {
 		const project = await this.projectRepository.findOne({
 			where: { id: list.projectId },
